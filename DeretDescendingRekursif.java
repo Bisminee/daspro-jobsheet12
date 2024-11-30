b32f212a08ebfff6af387fcb050fb7ce5b39372e
Bisminee <fiksilmiah@gmail.com>
2024-11-30 13:03:12 +0700

    Rekursif

--- DeretDescendingRekursif.java
+++ DeretDescendingRekursif.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class DeretDescendingRekursif {
     static int deretRekursif(int n){
-        int temp = n;
+        int temp;
         if(n == 0){
             temp = 0;
         }else{
@@ -11,6 +11,12 @@ public class DeretDescendingRekursif {
         return temp;
     }
 
+    static void deretIteratif(int n){
+        for (int i = n; i >= 0; i--){
+            System.out.print(i + " " );
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int angka;
@@ -18,6 +24,9 @@ public class DeretDescendingRekursif {
         System.out.print("Masukkan angka awal: ");
         angka = sc.nextInt();
 
-        System.out.print(deretRekursif(angka));
+        System.out.print(deretRekursif(angka) + " Ini Rekursif");
+        System.out.println();
+        deretIteratif(angka);
+        System.out.print("Ini Iteratif");
     }
 }
